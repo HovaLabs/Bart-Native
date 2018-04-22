0ac5e5048e7e9fc01ffcf5dbfbadd089f4f0ecf0
Kaitlyn Hova <kaitlynhova@gmail.com>
2018-04-21 20:38:30 -0700

    adding image to header

--- app/Router.js
+++ app/Router.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Scene, Router, Actions } from 'react-native-router-flux';
-import { Platform } from 'react-native';
+import { Platform, Image, Dimensions } from 'react-native';
 
 import Station from './components/Station';
 import StationList from './components/StationList';
+
+const { height, width } = Dimensions.get('window');
+
+console.log('sweet dimensions', height, width);
 
 const styles = {
   navBar: {
@@ -13,10 +17,18 @@ const styles = {
   sceneStyle: {
     paddingTop: Platform.OS === 'ios' ? 65 : 84,
   },
+  logoStyle: {
+    width: 150,
+    marginTop: 15,
+    marginBottom: 15,
+    marginLeft: 10,
+  },
 };
 
+const NavBar = () => <Image style={styles.logoStyle} source={require('./logo.png')} />;
+
 const RouterComponent = props => (
-  <Router navigationBarStyle={styles.navBar} sceneStyle={styles.sceneStyle}>
+  <Router navBar={NavBar}>
     <Scene key="root" hideNavBar>
       <Scene key="main">
         <Scene key="stationList" component={StationList} title="Station List" initial />
